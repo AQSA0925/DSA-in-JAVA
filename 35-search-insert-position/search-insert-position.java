7a58e4666648e697afb859f54cc0666626a0fd7b
AQSA <khanaqsa2509@gmail.com>
2026-02-16 18:27:18 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (26.84%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,21 +1,24 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int n = nums.length;
-        int start = 0 ; 
-        int end = n-1;
-        while(start <= end){
-            int mid = start + (end-start)/2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if (nums[mid] < target){
-                start = mid + 1;
-            }
-            else {
-                end = mid - 1;
-            }
+        return insertPosition(nums,0,nums.length-1,target);
+    }
+    public int insertPosition(int[]nums, int start, int end, int target){
+        //base case:
+        if(start > end){
+            return start;
         }
-        return start;
-        
+        //condition:
+        int mid = start + (end-start)/2;
+
+        //recursive call:
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(nums[mid] < target){
+           return insertPosition(nums,mid+1,end,target);
+        }
+        else {
+            return insertPosition(nums,start,mid-1,target);
+        }
     }
 }
