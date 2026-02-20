91ea93be07f35b3d9d0e7a668b7a29de400a9660
AQSA <khanaqsa2509@gmail.com>
2026-02-20 09:18:49 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (45.33%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,21 +1,23 @@
 class Solution {
-    public int search(int[] arr, int target) {
-       int n = arr.length ;
-       int lo = 0, hi = n-1;
+    public int search(int[] arr, int t) {
+        int start = 0, end = arr.length-1;
+        while(start<=end){
+            int mid  = start+(end-start)/2;
+            if(arr[mid] == t) return mid;
+            
+            //step 1 : we will identify which side is sorted
+            //check left half is sorted or not;
 
-       while(lo<=hi){
-        int mid = lo + (hi-lo)/2;
-        if(arr[mid] == target) return mid;
-        else if (arr[mid] <= arr[hi]){ // i am in rigth sorted array.
-           if(target > arr[mid] && target <= arr[hi]) lo=mid+1;
-           else hi = mid-1;
-        }
-        else if (arr[mid] > arr[hi]) {
-            // im in leftt sorted array 
-            if(arr[lo]<=target && target <= arr[mid]) hi = mid-1;
-            else lo =mid +1;
-        }
-       }
-       return -1;
-    }
+            else if(arr[start] <= arr[mid]){
+                if(arr[start]<=t && t < arr[mid])
+                   end = mid -1;
+                else start = mid + 1;
+            } 
+            else if (arr[mid] <t && t <= arr[end]){
+                   start = mid + 1;
+                   }
+            else end = mid-1;
+            }
+        return-1;
+}
 }
