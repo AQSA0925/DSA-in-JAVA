2f43a32e36097722c1c3b5832e6e4ce2b9170835
AQSA <khanaqsa2509@gmail.com>
2026-02-28 23:17:01 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (28.81%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -1,14 +1,13 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
         ListNode curr = head;
-
+        ListNode prev = null;
         while(curr != null){
-            ListNode temp = curr.next;
+            ListNode Next = curr.next;
             curr.next = prev;
             prev = curr;
-            curr = temp;
-        }
+            curr = Next;
+        }    
         return prev;
-    }
+     }
 }
