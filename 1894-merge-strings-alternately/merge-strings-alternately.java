12cdfa93163756450e43284a1fd972a66ef1151b
AQSA <khanaqsa2509@gmail.com>
2026-05-04 13:22:29 +0530

    Time: 1 ms (95.34%) | Memory: 42.7 MB (96.88%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,25 +1,19 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        char[]a1 = word1.toCharArray();
-        char[]a2 = word2.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int n1 = word1.length();
+        int n2 = word2.length();
 
-        int n1 = a1.length;
-        int n2 = a2.length;
-
-        int i=0; //for n1 & n2
-        int k=0; // for merged array;
-
-        char[] merged = new char [n1 + n2];
-
-        while(i<n1 || i<n2){
-            if(i < n1){
-                merged [k++] = a1[i];
+        for (int i = 0; i < Math.max(n1, n2); i++){
+            if (i < n1) {
+                sb.append(word1.charAt(i));
             }
-            if( i < n2){
-                merged [k++] = a2[i];
+            
+            
+            if (i < n2 ) {
+                sb.append(word2.charAt(i));
             }
-            i++;
         }
-        return new String(merged);
+            return sb.toString();
     }
 }
