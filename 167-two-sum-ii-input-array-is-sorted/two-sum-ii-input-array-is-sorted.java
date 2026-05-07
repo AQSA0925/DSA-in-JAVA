d6c1f118f8b41232b7ba79380c9b8c75330e896d
AQSA <khanaqsa2509@gmail.com>
2026-05-07 23:25:09 +0530

    Time: 2 ms (96.46%) | Memory: 48.4 MB (41.60%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,16 +1,44 @@
+// //Two pointer T.C = O(n^2)
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+//         int []ans = new int [2];
+//         int n = numbers.length;
+
+//         for(int i=0; i<n; i++){
+//             for(int j = i+1; j<n; j++){
+//                 if(numbers[i]+numbers[j] == target){
+//                     ans[0] = i+1;
+//                     ans[1] = j+1;
+//                     return ans ;
+//                 }
+//             }
+//         }
+//         return ans ;
+//     }
+// }
+// //
+
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int []ans = new int [2];
+     public int[] twoSum(int[] numbers, int target){
         int n = numbers.length;
-        for(int i=0; i<n; i++){
-            for(int j = i+1; j<n; j++){
-                if(numbers[i]+numbers[j] == target){
-                ans[0] = i+1;
-                ans[1] = j+1;
-                return ans ;
-                }
+        int i=0;
+        int j = n-1;
+
+        while(i<j){
+            int sum = (numbers[i] + numbers[j]);
+            if(sum == target){
+                return new int []{i+1,j+1};
+            }
+            if(sum > target){
+              j--;
+            }
+            if(sum < target){
+                i++;
             }
         }
-        return ans ;
-    }
-}
+        return new int []{-1,-1};
+        }
+     }
+
+
+
