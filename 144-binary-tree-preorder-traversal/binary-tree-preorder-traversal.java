f7dc3a9a9a5e6a324b80270997388614c9fb5f28
AQSA <khanaqsa2509@gmail.com>
2026-09-01 10:53:09 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (70.40%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -14,14 +14,14 @@
  * }
  */
 class Solution {
-    private void preorder(TreeNode root, List<Integer>ans){
-        if(root == null) return;
+    private void preorder(TreeNode root,List<Integer> ans){
+        if(root == null) return ;
         ans.add(root.val);
         preorder(root.left,ans);
         preorder(root.right,ans);
     }
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer>ans = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
         preorder(root,ans);
         return ans;
     }
