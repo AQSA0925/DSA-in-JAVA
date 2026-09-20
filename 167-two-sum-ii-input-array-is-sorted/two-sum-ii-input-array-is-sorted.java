570eef0548422d955a8b47bb40189ded8f5ec777
AQSA <khanaqsa2509@gmail.com>
2026-09-20 11:51:50 +0530

    Time: 2 ms (96.02%) | Memory: 48.3 MB (60.97%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,25 +1,5 @@
-// //Two pointer T.C = O(n^2)
-// class Solution {
-//     public int[] twoSum(int[] numbers, int target) {
-//         int []ans = new int [2];
-//         int n = numbers.length;
-
-//         for(int i=0; i<n; i++){
-//             for(int j = i+1; j<n; j++){
-//                 if(numbers[i]+numbers[j] == target){
-//                     ans[0] = i+1;
-//                     ans[1] = j+1;
-//                     return ans ;
-//                 }
-//             }
-//         }
-//         return ans ;
-//     }
-// }
-// //
-
 class Solution {
-     public int[] twoSum(int[] numbers, int target){
+    public int[] twoSum(int[] numbers, int target) {
         int n = numbers.length;
         int i=0;
         int j = n-1;
@@ -37,8 +17,5 @@ class Solution {
             }
         }
         return new int []{-1,-1};
-        }
-     }
-
-
-
+    }
+}
